26017d14c47b56cb8d38402fd31d9fd4c4c70de6
HieuNM <nmhieucoder@gmail.com>
2022-05-29 16:43:14 +0700

    push code nhắn tin, trang thái user đang online, user đang gõ chữ
    Buổi 3 - Build web real time với NODEJS + SOCKETIO

--- Learn/Buổi 3 - Build web real time với NODEJS + SOCKETIO/index.js
+++ Learn/Buổi 3 - Build web real time với NODEJS + SOCKETIO/index.js
@@ -7,12 +7,58 @@ app.set("views","./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+var arrayUser = []; 
+var arrayListUSerTyping = []; 
+
 io.on("connection" ,function (socket) {
     console.log("co ng truy cap: " + socket.id);
+    console.log('arrayUser : ' + arrayUser);
 
+    socket.on('client-send-Username',(user)=>{
+        if(arrayUser.indexOf(user) >= 0){
+            // gửi thông báo đến người đăng ký thất bại
+            socket.emit('server-send-dki-thatbai',{'status': 1, 'messge': 'Username đã được sử dụng'});
+        }else{
+            // gửi đăng ký thành công
+            arrayUser.push(user);
+            socket.userName = user;
+            // thông báo cho người đăng ký thành công
+            socket.emit('server-send-dki-thanhcong', {'status': 1, 'user': user });
+            io.sockets.emit('server-send-danhsach-Users', {'status': 1, 'listUser':arrayUser});
+        }
+    });
 
+    socket.on('client-send-messge', (msg)=>{
+        console.log(msg);
+        socket.emit('server-send-messge-to-me', {'status': 1, 'message':msg , 'user': socket.userName});
+        socket.broadcast.emit('server-send-messge-broadcast', {'status': 1, 'message':msg, 'user': socket.userName});
+    });
+
+    socket.on('logout', ()=>{
+        arrayUser.splice(
+            arrayUser.indexOf(socket.userName),1
+        );
+        socket.broadcast.emit('server-send-danhsach-Users', {'status': 1, 'listUser':arrayUser});
+    });
+
+    socket.on('typing', function () {
+        arrayListUSerTyping.push(socket.userName); 
+        socket.broadcast.emit('server-send-typing', {'listUserTyping':arrayListUSerTyping});
+    })
+    socket.on('typing-close', function () {
+        console.log(arrayListUSerTyping);
+        arrayListUSerTyping.splice(
+            arrayListUSerTyping.indexOf(socket.userName),1 
+        );
+        console.log(arrayListUSerTyping);
+        socket.broadcast.emit('server-send-typing', {'listUserTyping':arrayListUSerTyping});
+    })
 
     socket.on("disconnect",function () {
+        arrayUser.splice(
+            arrayUser.indexOf(socket.userName),1
+        );
+        socket.broadcast.emit('server-send-danhsach-Users', {'status': 1, 'listUser':arrayUser});
         console.log(socket.id + " ngat ket noiiiii");
     });
 })
