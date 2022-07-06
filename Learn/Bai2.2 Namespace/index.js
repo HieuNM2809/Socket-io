4cc7e88f8fc1fa0bdf6fc15207d7d9eb5c7522d7
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-07-06 16:41:06 +0700

    update

--- Learn/Bai2.2 Namespace/index.js
+++ Learn/Bai2.2 Namespace/index.js
@@ -6,35 +6,48 @@ app.set("views","./views");
 
 var server = require("http").Server(app);
 const io  = require('socket.io')(server, {
-    cors: {
-        credentials: true,
-        origin: "*",
-        methods: ["GET", "POST"]
-    },
-    allowRequest: (req, callback) => {
-        var date1 = new Date();
-        var month = date1.getSeconds();
-        console.log(month);
-        callback(null, true);
-    }
+    // cors: {
+    //     credentials: true,
+    //     origin: "*",
+    //     methods: ["GET", "POST"]
+    // },
+    // allowRequest: (req, callback) => {
+    //     var date1 = new Date();
+    //     var month = date1.getSeconds();
+    //     console.log(month);
+    //     callback(null, true);
+    // }
 });
-
-// namespace 1
-const nsp1 = io.of("/my-namespace1");
-
+const nsp1 = io.of("/my-namespace");
 nsp1.on("connection", socket => {
-    console.log("my-namespace1");
-    nsp1.emit("hi", "everyone!");
+    console.log(socket.id);
+    nsp1.emit("hi", "everyone!"); 
+    setInterval(() => {
+        nsp1.emit("hi", "everyone!"); 
+    }, 1000);
+    socket.on("disconnect",function () {
+        console.log(socket.id + " ngat ket noiiiii");
+    });
 });
 
 
-// namespace 2
-const nsp2 = io.of("/my-namespace2");
 
-nsp2.on("connection", socket => {
-    console.log("my-namespace2");
-    nsp2.emit("hi", "everyone!");
-});
+// // namespace 1
+// const nsp1 = io.of("/my-namespace1");
+
+// nsp1.on("connection", socket => {
+//     console.log("my-namespace1");
+//     nsp1.emit("hi", "everyone!");
+// });
+
+
+// // namespace 2
+// const nsp2 = io.of("/my-namespace2");
+
+// nsp2.on("connection", socket => {
+//     console.log("my-namespace2");
+//     nsp2.emit("hi", "everyone!");
+// });
 
 
 
