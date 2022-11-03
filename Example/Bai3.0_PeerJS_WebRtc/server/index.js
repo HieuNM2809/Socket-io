5481c11c4dc8d44069b9c694bf0c50aa1af6e6c7
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-11-03 10:43:44 +0700

    .

--- Example/Bai3.0_PeerJS_WebRtc/server/index.js
+++ Example/Bai3.0_PeerJS_WebRtc/server/index.js
@@ -15,7 +15,8 @@ app.get("/", (request, response) => {
 });
 
 // listen for requests :)
-const listener = app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+const listener = app.listen(port, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
 
